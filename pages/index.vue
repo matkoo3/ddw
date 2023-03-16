1f53520c2e5612b032a63ae108a0f468ca09fb49
matkoo3 <119047877+matkoo3@users.noreply.github.com>
2023-03-16 20:01:00 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,16 +4,10 @@
 <template>
   <div>
     <MyHeader></MyHeader>
+    
     <MyFooter></MyFooter>
   </div>
 </template>
 
 <style scoped>
-.header{
-  padding: 60px;
-  text-align: center;
-  background: #1abc9c;
-  color: white;
-  font-size: 30px; 
-}
 </style>
