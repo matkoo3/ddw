225e15885acd14ff09fd9079b61067fe47521067
matkoo3 <119047877+matkoo3@users.noreply.github.com>
2023-03-17 12:30:50 +0100

    Update [id].vue

--- pages/articles/[id].vue
+++ pages/articles/[id].vue
@@ -1,18 +1,23 @@
 <script setup>
 const id = useRoute().params.id
 const articles = useArticles().list.array
-const nem = useRoute()
+const article = articles.filter(item => item.id == id)[0]
+
 </script>
 
 <template>
   <div>
     <MyHeader></MyHeader>
-    <pre>
-      {{articles.findLast(item => item.id == id)}}
+    <div>
+    <pre class="clanok">
+      <h1>{{article.title}}</h1>
+      <div>{{article.text}}</div>
     </pre>
+    </div>
     <MyFooter></MyFooter>
   </div>
   
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
