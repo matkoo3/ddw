c6c633af1dcdc3da0883eefa4b42adb918d09fc7
matkoo3 <119047877+matkoo3@users.noreply.github.com>
2023-03-23 19:02:03 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,18 @@
 <script setup>
+let articles = useArticles().list.array;
 </script>
 
 <template>
   <div>
     <MyHeader></MyHeader>
-    <div>Sport</div>
-    <div>Politika</div>
-    <div>Hry</div>
+    <div class="article" v-for="article of articles" v-bind:key="article">
+          <div style="font-size: 35px; font-weight: bold;">{{ article.title }}</div>
+          <div><p style="font-size: 24px; text-align: justify;">{{ article.text.substring(0, 300) }}</p></div>
+          <div>
+            <div style="color: #CECECE;">ID: {{ article.id }}</div>
+            <div><NuxtLink :to="{path:`/articles/${article.id}`, params:{id:article.id}}">detail</NuxtLink></div>
+          </div>
+        </div>
     <MyFooter></MyFooter>
   </div>
 </template>
